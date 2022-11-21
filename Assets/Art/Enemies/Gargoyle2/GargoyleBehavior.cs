64c350e33616399102f001d92014cb918b95df75
Ian Oberst <ioberst17@mail.wou.edu>
2022-11-21 11:19:55 -0800

    Got some basic setup done. Did all the gargoyle animations.

---
+++ Assets/Art/Enemies/Gargoyle2/GargoyleBehavior.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GargoyleBehavior : MonoBehaviour
+{
+    [SerializeField] private Transform topLeftPoint;
+    [SerializeField] private Transform bottomRightPoint;
+    public string bossState;
+    public int HP;
+    public int SoulPointsDropped;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
