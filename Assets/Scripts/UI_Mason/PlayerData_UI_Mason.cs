2f37a4da2f47dfb1ab44005732320447612465b6
marz-uw <marzuq@uw.edu>
2023-02-18 13:19:23 -0800

    Fixed build compilation issues, commented out debug.logs
    Key: uncommented missing namespace in PlayerData_UI_Mason.cs

--- Assets/Scripts/UI_Mason/PlayerData_UI_Mason.cs
+++ Assets/Scripts/UI_Mason/PlayerData_UI_Mason.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using TMPro;
-using UnityEditor.Experimental.GraphView;
+//using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UI;
 
