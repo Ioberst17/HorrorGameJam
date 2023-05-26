10d82d5d0ebf8d79d7eaf51b4c732ef5b175380b
Thomas Lamb <thomas.s.lamb@gmail.com>
2023-05-25 21:05:31 -0700

    Removing test from Mason UI
    Commenting out test section lines 176-177 of Inventory_UI_Mason so my build can pass.

--- Assets/Scripts/UI_Mason/Inventory_UI_Mason.cs
+++ Assets/Scripts/UI_Mason/Inventory_UI_Mason.cs
@@ -172,9 +172,9 @@ public class Inventory_UI_Mason : MonoBehaviour
         consumableCount.Add(consumable_count_slot1);
         consumableCount.Add(consumable_count_slot2);
 
-        //adding in a narrative and consumable item for testing
-        narrativeItemsManager.AddItem(1);
-        consumablesManager.AddNewItemToInv(1, 1);
+        ////adding in a narrative and consumable item for testing
+        //narrativeItemsManager.AddItem(1);
+        //consumablesManager.AddNewItemToInv(1, 1);
 
 
 
