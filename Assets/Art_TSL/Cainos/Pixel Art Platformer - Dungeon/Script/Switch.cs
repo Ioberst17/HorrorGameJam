51a603ccf1f9799dee1cc6411680d480890cf479
Thomas Lamb <thomas.s.lamb@gmail.com>
2023-06-08 19:59:44 -0700

    Fix Build Break Issues

--- Assets/Art_TSL/Cainos/Pixel Art Platformer - Dungeon/Script/Switch.cs
+++ Assets/Art_TSL/Cainos/Pixel Art Platformer - Dungeon/Script/Switch.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cainos.LucidEditor;
-using UnityEditor.SceneManagement;
+// using UnityEditor.SceneManagement;
 using UnityEditor;
 
 namespace Cainos.PixelArtPlatformer_Dungeon
@@ -43,7 +43,7 @@ namespace Cainos.PixelArtPlatformer_Dungeon
                 if (Application.isPlaying == false)
                 {
                     EditorUtility.SetDirty(this);
-                    EditorSceneManager.MarkSceneDirty(gameObject.scene);
+                    // EditorSceneManager.MarkSceneDirty(gameObject.scene);
                 }
                 #endif
 
